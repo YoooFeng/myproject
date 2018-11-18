852089d87ba7c4642d01e4f6f2ff82d8454be523
YoooFeng <250970437@qq.com>
2018-11-18 21:58:07 +0800

    .ignore modified.

--- src/main/java/com/iscas/yf/IntelliPipeline/common/service/user/security/authorization/GudgeonHandlerInterceptor.java
+++ src/main/java/com/iscas/yf/IntelliPipeline/common/service/user/security/authorization/GudgeonHandlerInterceptor.java
@@ -1,6 +1,7 @@
 package com.iscas.yf.IntelliPipeline.common.service.user.security.authorization;
 
-import org.springframework.web.portlet.handler.HandlerInterceptorAdapter;
+// import org.springframework.web.portlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class GudgeonHandlerInterceptor extends HandlerInterceptorAdapter {
 }
